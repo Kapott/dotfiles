390bc4ae2a9508ea689770d3f0dd83a5e60f1bb4
kapott <kapott@aivd.33mail.com>
2018-10-16 16:05:10 +0200

    Switch capslock and escape when xorg starts

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 command -v stow > /dev/null 2>&1 || { echo >&2 "This installer requires GNU Stow to be installed!"; exit 1;}
 
-PACKAGES=("bash" "git" "vim" "tmux" "i3" "curl")
+PACKAGES=("bash" "xorg" "git" "vim" "tmux" "i3" "curl")
 for p in "${PACKAGES[@]}"
 do
 	stow ${p}
