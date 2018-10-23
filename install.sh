f7819d1b1e5a1630ba5aad2e9055dee541fff764
kapott <kapott@aivd.33mail.com>
2018-10-23 17:08:57 +0200

    change font to Hack

--- install.sh
+++ install.sh
@@ -7,3 +7,14 @@ for p in "${PACKAGES[@]}"
 do
 	stow ${p}
 done
+
+if [ ! -d "$HOME/.fonts" ]; then
+	mkdir -p $HOME/.fonts
+fi
+
+# Get the "Hack" font from github
+wget -O $HOME/.fonts/Hack-Regular.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-Regular.ttf
+wget -O $HOME/.fonts/Hack-Bold.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-Bold.ttf
+wget -O $HOME/.fonts/Hack-Italic.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-Italic.ttf
+wget -O $HOME/.fonts/Hack-BoldItalic.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-BoldItalic.ttf
+fc-cache
