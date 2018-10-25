0c475ac44dc01efacada934e099f967d5cbf29c6
kapott <kapott@aivd.33mail.com>
2018-10-25 22:32:49 +0200

    Added todotxt-machinerc settings (py)

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 command -v stow > /dev/null 2>&1 || { echo >&2 "This installer requires GNU Stow to be installed!"; exit 1;}
 
-PACKAGES=("bash" "xorg" "git" "vim" "tmux" "i3" "curl")
+PACKAGES=("bash" "xorg" "git" "vim" "tmux" "i3" "curl" "todo")
 for p in "${PACKAGES[@]}"
 do
 	stow ${p}
@@ -13,8 +13,9 @@ if [ ! -d "$HOME/.fonts" ]; then
 fi
 
 # Get the "Hack" font from github
-wget -O $HOME/.fonts/Hack-Regular.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-Regular.ttf
-wget -O $HOME/.fonts/Hack-Bold.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-Bold.ttf
-wget -O $HOME/.fonts/Hack-Italic.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-Italic.ttf
-wget -O $HOME/.fonts/Hack-BoldItalic.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-BoldItalic.ttf
-fc-cache
+# Deprecated, just install the powerline fonts!
+# wget -O $HOME/.fonts/Hack-Regular.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-Regular.ttf
+# wget -O $HOME/.fonts/Hack-Bold.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-Bold.ttf
+# wget -O $HOME/.fonts/Hack-Italic.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-Italic.ttf
+# wget -O $HOME/.fonts/Hack-BoldItalic.ttf https://raw.githubusercontent.com/source-foundry/Hack/master/build/ttf/Hack-BoldItalic.ttf
+# fc-cache
