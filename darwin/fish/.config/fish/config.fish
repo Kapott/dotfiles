7bfab29c8a38beb9d38c3e2d83e9b032344183aa
kapott <kapott@aivd.33mail.com>
2021-11-04 09:03:40 +0100

    Add dirs to $PATH.

--- darwin/fish/.config/fish/config.fish
+++ darwin/fish/.config/fish/config.fish
@@ -1,3 +1,7 @@
+test -d ~/bin; and fish_add_path ~/bin
+test -d /usr/local/bin; and fish_add_path /usr/local/bin
+test -d /usr/local/sbin; and fish_add_path /usr/local/sbin
+
 for file in aliases
 	test -f ~/.config/fish/$file.fish && source ~/.config/fish/$file.fish
 end
