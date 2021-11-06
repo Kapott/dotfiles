672c01c6f1f867a024c383013d39b71165277a62
kapott <Kapott@users.noreply.github.com>
2021-11-06 11:18:54 +0000

    Fix ssh environment bug.

--- installers/platform/linux/linux.sh
+++ installers/platform/linux/linux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env
 
 test -f /etc/os-release && source /etc/os-release
+mkdir -p $HOME/.ssh
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 test -n "$ID" && source "${SCRIPT_DIR}/${ID}/${ID}.sh"
