dded384cf99385837f478c3873141d7d016bc579
kapott <kapott@aivd.33mail.com>
2022-03-30 02:24:30 -0700

    Add an echo so we can see what the script sources.

--- installers/platform/linux/ubuntu/ubuntu.sh
+++ installers/platform/linux/ubuntu/ubuntu.sh
@@ -14,10 +14,11 @@ sudo apt-get update && sudo apt-get install --ignore-missing -y \
  jq
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo ${SCRIPT_DIR}/server/ubuntu-server.sh
 
 if [ -z "${DISPLAY}" ]; then
+	echo "-> Ubuntu server"
 	test -f "${SCRIPT_DIR}/server/ubuntu-server.sh" && source "${SCRIPT_DIR}/server/ubuntu-server.sh"
 else
+	echo "-> Ubuntu desktop"
 	test -f "${SCRIPT_DIR}/desktop/ubuntu-desktop.sh" && source "${SCRIPT_DIR}/desktop/ubuntu-desktop.sh"
 fi
