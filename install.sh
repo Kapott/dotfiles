b3b2a04c8f60a888840e84f0c1e7396639fbc40b
kapott <kapott@aivd.33mail.com>
2022-04-11 16:23:52 +0200

    Comment the installer a bit

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@
 main () {
 	local platform git_root
 
-	# Check which platform we're currently running
+	# Check which platform we're currently running on, normalize to lowercase.
 	platform=$(command uname | tr '[:upper:]' '[:lower:]')
 
-	# Optionally execute an installer for this platform
+	# Check if we're installing or reinstalling, and only continue with a
+	# reinstall if we explicitly type 'y' or 'Y'.
 	if test -f "$HOME/.installed"; then
 		read -p "An existing installation was detected. Reinstall? " -n 1 -r
 		if [[ ! $REPLY =~ ^[Yy]$ ]]; then
@@ -13,18 +14,19 @@ main () {
 		fi
 	fi
 
-	printf "\n"
+	# Remove our 'hey, we installed this stuff already' lock file.
 	test -f "$HOME/.installed" && rm "$HOME/.installed"
-	printf "[+]\tInstalling on platform: %s..\n\n" "${platform}"
+
+	# Source the appropriate file for the current platform.
+	printf "\n[+]\tInstalling on platform: %s..\n\n" "${platform}"
 	test -f "installers/platform/${platform}/${platform}.sh" && source "installers/platform/${platform}/${platform}.sh"
 
-
-
-	# Make sure we're at the root of our project
+	# cd to the root of our project
 	git_root=$(git rev-parse --show-toplevel)
 	cd "${git_root}"
 
-	# Change some tokens, depending on the OS we're on.
+	# Figure out which git-helper we're going to use later on. This is platform
+	# dependant, hence the circus.
 	case $platform in
 		"linux")
 			helper="/usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret"
@@ -37,15 +39,24 @@ main () {
 			;;
 	esac	
 
-	# Install all of the dotfiles we know of for this platform
+	# Change working directory to the platform's directoy
 	pushd "${platform}"
+
+	# Adjust our gitconfig, plopping in the correct helper.
 	sed -E "s|helper = .*|helper = ${helper}|" ../common/git/dot-gitconfig > ../common/git/dot-gitconfig2
 	mv -f ../common/git/dot-gitconfig2 ../common/git/dot-gitconfig
-	rm $HOME/.config/fish/config.fish
+
+	# rm $HOME/.config/fish/config.fish
+
+	# Tell stow to symlink our dotfiles, and change dot-{file|dir} to .{file|dir}
+	# and, if everything is succesful, boot up vim and tell it to install all of
+	# our crap.
 	stow --dotfiles -vRt "$HOME" * && \
 		stow -vRt "$HOME" fish && \
 		touch "$HOME/.installed" && \
 		vim +PluginInstall +qall
+
+	# Change back directory to what should have been the git root.
 	popd
 
 	# Install fish plugins
